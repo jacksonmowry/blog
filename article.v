c13ca9294803815cf983f17eb189591e82b5373b
Jackson Mowry <jacksonm@duck.com>
2023-07-05 06:50:56 -0400

    features out the wazoo, outgrowing picocss, switching to tailwindcss

--- article.v
+++ article.v
@@ -1,47 +1,69 @@
 module main
 
 struct Article {
-	id       int       [primary; sql: serial]
-	title    string
-	text     string
-	author   string
+	id     int    [primary; sql: serial]
+	title  string
+	text   string
+	author string
+mut:
+	views int
+pub:
+	likes    int
+	dislikes int
 	comments []Comment [fkey: 'article_id']
-	// Add abstract for the link
 }
 
 struct Comment {
-	id         int    [primary; serial; sql]
+	id         int    [primary; serial: sql]
 	article_id int
 	author     string
 	text       string
 }
 
 struct User {
-	id    int    [primary; serial; sql]
-	uname string [nonull; unique]
-	dname string [nonull]
-	pword string [nonull]
+	id       int        [primary; serial: sql]
+	uname    string     [nonull; unique]
+	dname    string     [nonull]
+	pword    string     [nonull]
+	likes    []Likes    [fkey: 'user_id']
+	sessions []Sessions [fkey: 'user_id']
+}
+
+struct Likes {
+	id         int    [primary; sql: serial]
+	user_id    int
+	username   string
+	article_id int
+}
+
+struct Sessions {
+	id            int    [primary; sql: serial]
+	user_id       int
+	session_token string [sql_type: 'uuid']
+	username      string
 }
 
 pub fn (app &App) find_all_articles() []Article {
 	articles := sql app.db {
 		select from Article order by id desc
-	} or { []Article{} }
+	} or { []Article{len: 1} }
 	return articles
 }
 
-pub fn (app &App) find_latest_article() ?Article {
-	println(app.db.last_id())
-	article := sql app.db {
+pub fn (mut app App) find_latest_article() ?Article {
+	mut article := sql app.db {
 		select from Article order by id desc limit 1
 	} or { []Article{} }
 	if article.len == 0 {
 		return none
 	}
+	app.view(article[0].id)
+	article[0].views += 1
 	return article[0]
 }
 
-pub fn (app &App) find_article_by_id(article_id int) ?Article {
+pub fn (mut app App) find_article_by_id(article_id int) ?Article {
+	app.view(article_id)
 	article := sql app.db {
 		select from Article where id == article_id limit 1
 	} or { []Article{} }
@@ -59,4 +81,21 @@ pub fn (app &App) find_articles_by_author(author string) ?[]Article {
 		return none
 	}
 	return articles
+}
+
+pub fn (app &App) has_liked(id int) bool {
+	mut liked := false
+	for _, l in app.user_struct.likes {
+		if l.article_id == id {
+			liked = true
+		}
+	}
+
+	return liked
+}
+
+pub fn (mut app App) view(id int) {
+	sql app.db {
+		update Article set views = views + 1 where id == id
+	} or {}
 }
--- blog.v
+++ blog.v
@@ -2,26 +2,36 @@ module main
 
 import vweb
 import db.sqlite
+import rand
+import crypto.bcrypt
 
 struct App {
 	vweb.Context
 pub mut:
-	db        sqlite.DB
-	user_id   string
-	logged_in bool
+	db          sqlite.DB
+	username    string
+	logged_in   bool
+	user_struct User
 }
 
 fn main() {
 	mut app := &App{
-		db: sqlite.connect(':memory:') or { panic(err) }
-		// db: sqlite.connect('blog.db') or { panic(err) }
+		// db: sqlite.connect(':memory:') or { panic(err) }
+		db: sqlite.connect('blog.db') or { panic(err) }
 	}
 
 	sql app.db {
 		create table Article
 		create table Comment
 		create table User
+		create table Likes
+		create table Sessions
 	} or { println('cannot create tables in db') }
+
+	//
+	// Testing
+	app.serve_static('/modal_test.css', 'modal_test.css')
+	//
 
 	vweb.run(app, 8081)
 }
@@ -33,13 +43,15 @@ pub fn (app &App) index() vweb.Result {
 }
 
 ['/latest']
-pub fn (app &App) latest() vweb.Result {
+pub fn (mut app App) latest() vweb.Result {
 	article := app.find_latest_article() or { Article{} }
+	liked := app.has_liked(article.id)
 	return $vweb.html()
 }
 
 ['/article/:id']
-pub fn (app &App) article(id int) vweb.Result {
+pub fn (mut app App) article(id int) vweb.Result {
+	liked := app.has_liked(id)
 	article := app.find_article_by_id(id) or { Article{} }
 	return $vweb.html()
 }
@@ -47,9 +59,14 @@ pub fn (app &App) article(id int) vweb.Result {
 ['/author/:name']
 pub fn (mut app App) author(name string) vweb.Result {
 	articles := app.find_articles_by_author(name) or { []Article{} }
-	if articles.len == 0 {
-		app.redirect('/')
-	}
+
+	// find articles liked by authors id not viewers id
+	author := sql app.db {
+		select from User where uname == name
+	} or { return app.redirect('/') }
+
+	liked := author[0].likes
+
 	return $vweb.html()
 }
 
@@ -78,18 +95,39 @@ pub fn (mut app App) signup_form(username string, display_name string, password 
 		return app.text('Empty/Invalid Field')
 	}
 
+	// hashing password
+	hash := bcrypt.generate_from_password(password.bytes(), 10) or {
+		return app.text('Password hashing failed')
+	}
+
 	user := User{
 		uname: username
 		dname: display_name
-		pword: password
+		pword: hash
 	}
 
-	println(user)
 	sql app.db {
 		insert user into User
 	} or { return app.text('Username already exists') }
 
+	id_check := sql app.db {
+		select from User where uname == username
+	} or { return app.server_error(500) }
+	id := id_check[0].id
+
 	app.set_cookie(name: 'login', value: 'true')
+
+	uuid := rand.uuid_v4()
+	session := Sessions{
+		user_id: id
+		session_token: uuid
+		username: username
+	}
+	sql app.db {
+		insert session into Sessions
+	} or {}
+	app.set_cookie(name: 'session_token', value: uuid)
+
 	return app.redirect('/')
 }
 
@@ -114,35 +152,55 @@ pub fn (mut app App) login_form(username string, password string) vweb.Result {
 		select from User where uname == username limit 1
 	} or { return app.redirect('/login') }
 
-	if user[0].pword != password {
-		app.redirect('/login')
+	if user.len == 0 {
+		return app.redirect('/login')
+	}
+	bcrypt.compare_hash_and_password(password.bytes(), user[0].pword.bytes()) or {
+		return app.redirect('/login')
 	}
 
 	app.set_cookie(name: 'login', value: 'true')
+
+	uuid := rand.uuid_v4()
+	session := Sessions{
+		user_id: user[0].id
+		session_token: uuid
+		username: username
+	}
+	sql app.db {
+		insert session into Sessions
+	} or {}
+	app.set_cookie(name: 'session_token', value: uuid)
 	return app.redirect('/')
 }
 
 ['/signout']
 pub fn (mut app App) signout() vweb.Result {
+	session_t := app.get_cookie('session_token') or { '' }
+	if session_t == '' {
+		return app.redirect('/')
+	}
+	sql app.db {
+		delete from Sessions where session_token == session_t
+	} or { return app.redirect('/') }
 	app.set_cookie(name: 'login', value: 'false')
+	app.set_cookie(name: 'session_token', value: '')
 	return app.redirect('/')
 }
 
 ['/new_article'; post]
-pub fn (mut app App) new_article(title string, text string, author string) vweb.Result {
+pub fn (mut app App) new_article(title string, text string) vweb.Result {
 	if !app.logged_in {
 		return app.redirect('/login')
 	}
-	if title == '' || text == '' || author == '' {
+	if title == '' || text == '' {
 		return app.text('Empty text/title')
 	}
 	article := Article{
 		title: title
 		text: text
-		author: author
+		author: app.username
 	}
-	println('posting article')
-	println(article)
 	sql app.db {
 		insert article into Article
 	} or { return app.server_error(500) }
@@ -154,23 +212,83 @@ pub fn (mut app App) new_comment(article_id int, author string, text string) vwe
 	if !app.logged_in {
 		return app.redirect('/login')
 	}
-	if author == '' || text == '' || article_id == 0 {
+	if text == '' || article_id == 0 {
 		return app.text('Invalid Comment')
 	}
 	comment := Comment{
 		article_id: article_id
-		author: author
+		author: app.username
 		text: text
 	}
-	println('new comment')
-	println(comment)
 	sql app.db {
 		insert comment into Comment
 	} or { return app.server_error(500) }
 	return app.redirect('/article/${article_id}')
 }
 
+['/rss']
+pub fn (mut app App) rss() vweb.Result {
+	articles := app.find_all_articles()
+	app.add_header('Content-Type', 'application/xml')
+	return $vweb.html()
+}
+
+['/rss/:author']
+pub fn (mut app App) rssbyauthor(author string) vweb.Result {
+	articles := app.find_articles_by_author(author) or { []Article{} }
+	app.add_header('Content-Type', 'application/xml')
+	return $vweb.html()
+}
+
+['/:article_id/up']
+pub fn (mut app App) up(article_id int) vweb.Result {
+	articles_liked := app.user_struct.likes
+
+	for like in articles_liked {
+		if like.article_id == article_id {
+			sql app.db {
+				delete from Likes where username == app.username && article_id == article_id
+			} or { return app.text('removing like did not work') }
+
+			sql app.db {
+				update Article set likes = likes - 1 where id == article_id
+			} or { return app.text('reducing like did not work') }
+			return app.redirect('/article/${article_id}')
+		}
+	}
+
+	like := Likes{
+		user_id: app.user_struct.id
+		username: app.username
+		article_id: article_id
+	}
+	sql app.db {
+		insert like into Likes
+	} or { return app.text('adding like ref failed') }
+	sql app.db {
+		update Article set likes = likes + 1 where id == article_id
+	} or { return app.text('upping like count failed') }
+	return app.redirect('/article/${article_id}')
+}
+
 pub fn (mut app App) before_request() {
-	login_p := app.get_cookie('login') or { '0' }
-	app.logged_in = login_p == 'true'
+	// login_p := app.get_cookie('login') or { '0' }
+	// app.logged_in = login_p == 'true'
+	session_t := app.get_cookie('session_token') or { '' }
+	session := sql app.db {
+		select from Sessions where session_token == session_t limit 1
+	} or { []Sessions{} }
+	if session.len == 0 || session_t != session[0].session_token {
+		app.logged_in = false
+	} else {
+		app.username = session[0].username
+		app.logged_in = true
+	}
+
+	user_struct := sql app.db {
+		select from User where uname == app.username
+	} or { panic('sql bork') }
+	if user_struct.len > 0 {
+		app.user_struct = user_struct[0]
+	}
 }
